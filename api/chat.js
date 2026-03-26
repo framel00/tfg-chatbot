d37a907e4598dc439d56c028bdd648e823b19333
framel00 <fran.melgarejo.morales@gmail.com>
2026-03-26 17:25:35 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,17 +1,5 @@
-export default async function handler(req, res) {
-  try {
-    const { mensaje, caso, modo } = req.body;
-
-    return res.status(200).json({
-      reply: "TEST OK",
-      mensaje,
-      caso,
-      modo
-    });
-
-  } catch (error) {
-    return res.status(500).json({
-      error: error.message
-    });
-  }
+export default function handler(req, res) {
+  return res.status(200).json({
+    reply: "SOY EL ARCHIVO CORRECTO 🔥"
+  });
 }
